81092538d72d529c3212933fe292af702bcceb04
xiaomo <83387856@qq.com>
2016-04-25 18:00:09 +0800

    拦截一下空字符串

--- core/src/main/java/info/xiaomo/core/constant/WebDefaultValueConst.java
+++ core/src/main/java/info/xiaomo/core/constant/WebDefaultValueConst.java
@@ -18,5 +18,5 @@ public interface WebDefaultValueConst {
 
     String defaultImage = "https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png";
 
-    String imgBaseUrl = System.getProperty("user.dir") + "\\images\\";
+    String imgBaseUrl = System.getProperty("user.dir") + "/static/images";
 }

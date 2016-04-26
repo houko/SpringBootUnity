5ea5bca7b22a294f240b7fc2019a6807ac45c5b4
xiaomo <83387856@qq.com>
2016-04-26 13:55:27 +0800

    修改一下key的值

--- admin/src/main/java/info/xiaomo/admin/controller/SystemController.java
+++ admin/src/main/java/info/xiaomo/admin/controller/SystemController.java
@@ -30,17 +30,16 @@ public class SystemController extends BaseController {
     public HashMap<String, Object> getSystem() {
         Map<String, Object> map = new HashMap<>();
         Properties properties = System.getProperties();
-        map.put("java.version", properties.getProperty("java.version"));//运行时环境版本
-        map.put("java.vendor", properties.getProperty("java.vendor"));//Java 运行时环境供应商
-        map.put("java.vendor.url", properties.getProperty("java.vendor.url"));// Java 供应商的 URL
-        map.put("java.home", properties.getProperty("java.home"));//Java 安装目录
-        map.put("java.vm.name", properties.getProperty("java.version"));// Java 运行时环境规范版本
-        map.put("java.class.version", properties.getProperty("java.class.version"));//  Java 类格式版本号
-        map.put("os.name", properties.getProperty("os.name"));//  操作系统的名称
-        map.put("os.version", properties.getProperty("os.version"));//  操作系统的版本
-        map.put("user.name", properties.getProperty("user.name"));//  用户的账户名称
-        map.put("user.home", properties.getProperty("user.home"));//  用户的主目录
-        map.put("user.dir", properties.getProperty("user.dir"));//  用户的当前工作目录
+        map.put("javaVersion", properties.getProperty("java.version"));//运行时环境版本
+        map.put("javaVendor", properties.getProperty("java.vendor"));//Java 运行时环境供应商
+        map.put("javaVendorUrl", properties.getProperty("java.vendor.url"));// Java 供应商的 URL
+        map.put("javaHome", properties.getProperty("java.home"));//Java 安装目录
+        map.put("javaClassVersion", properties.getProperty("java.class.version"));//  Java 类格式版本号
+        map.put("osName", properties.getProperty("os.name"));//  操作系统的名称
+        map.put("osVersion", properties.getProperty("os.version"));//  操作系统的版本
+        map.put("userName", properties.getProperty("user.name"));//  用户的账户名称
+        map.put("useRHome", properties.getProperty("user.home"));//  用户的主目录
+        map.put("userDir", properties.getProperty("user.dir"));//  用户的当前工作目录
         result.put(systems, map);
         result.put(code, success);
         return result;

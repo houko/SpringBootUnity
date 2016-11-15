4d2f4a08af9a85ce5c90b637b23dae050708c971
xiaomo <83387856@qq.com>
2016-11-15 17:00:03 +0800

    spring安全

---
+++ security/src/main/java/info/xiaomo/security/SecurityMain.java
@@ -0,0 +1,62 @@
+package info.xiaomo.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * いま 最高の表現 として 明日最新の始発．．～
+ * Today the best performance  as tomorrow newest starter!
+ * Created by IntelliJ IDEA.
+ *
+ * @author: xiaomo
+ * @github: https://github.com/qq83387856
+ * @email: hupengbest@163.com
+ * @QQ_NO: 83387856
+ * @Date: 2016/4/1 15:38
+ * @Description: RabbitMq启动器
+ * @Copyright(©) 2015 by xiaomo.
+ **/
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("info.xiaomo")
+@EntityScan("info.xiaomo.*.model")
+@EnableJpaRepositories("info.xiaomo.*.dao")
+@EnableWebSecurity
+public class SecurityMain extends WebSecurityConfigurerAdapter {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SecurityMain.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+
+
+}
---
+++ security/src/main/java/info/xiaomo/security/controller/HelloController.java
@@ -0,0 +1,25 @@
+package info.xiaomo.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+}

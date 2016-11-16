660eb0bbfa226d3d406e2818e5ce0328f0945f19
xiaomo <83387856@qq.com>
2016-11-16 09:14:05 +0800

    mybatis

---
+++ mybatis/src/main/java/info/xiaomo/mybatis/MybatisMain.java
@@ -0,0 +1,33 @@
+package info.xiaomo.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * いま 最高の表現 として 明日最新の始発．．～
+ * Today the best performance  as tomorrow newest starter!
+ * Created by IntelliJ IDEA.
+ *
+ * @author: xiaomo
+ * @github: https://github.com/qq83387856
+ * @email: hupengbest@163.com
+ * @QQ_NO: 83387856
+ * @Date: 2016/4/1 15:38
+ * @Copyright(©) 2015 by xiaomo.
+ **/
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("info.xiaomo")
+@EntityScan("info.xiaomo.*.model")
+public class MybatisMain {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MybatisMain.class, args);
+    }
+
+
+}

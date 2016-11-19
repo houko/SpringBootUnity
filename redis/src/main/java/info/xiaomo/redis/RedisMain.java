4e1a95382e7a5e836d5f44505d751862ac555ed7
xiaomo <83387856@qq.com>
2016-11-19 11:25:53 +0800

    redis

--- redis/src/main/java/info/xiaomo/redis/RedisMain.java
+++ redis/src/main/java/info/xiaomo/redis/RedisMain.java
@@ -8,10 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 把今天最好的表现当作明天最新的起点．．～
@@ -31,6 +29,7 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 @EnableAutoConfiguration
 @ComponentScan("info.xiaomo")
 @EnableCaching
+@EnableScheduling
 public class RedisMain {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(RedisMain.class, args);
@@ -41,11 +40,11 @@ public class RedisMain {
         return new RedisCacheManager(redisTemplate);
     }
 
-    @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
-        final StringRedisTemplate template = new StringRedisTemplate(factory);
-        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class)); //请注意这里
-        return template;
-    }
+//    @Bean
+//    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+//        final StringRedisTemplate template = new StringRedisTemplate(factory);
+//        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class)); //请注意这里
+//        return template;
+//    }
 
 }

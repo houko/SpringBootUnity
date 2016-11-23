c5216a8cc853baf0eaab79f81d44f09a912903fa
xiaomo <83387856@qq.com>
2016-11-23 10:35:22 +0800

    view

---
+++ website/src/main/java/info/xiaomo/website/view/LinkView.java
@@ -0,0 +1,37 @@
+package info.xiaomo.website.view;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * いま 最高の表現 として 明日最新の始発．．～
+ * Today the best performance  as tomorrow newest starter!
+ * Created by IntelliJ IDEA.
+ *
+ * @author: xiaomo
+ * @github: https://github.com/qq83387856
+ * @email: hupengbest@163.com
+ * @QQ_NO: 83387856
+ * @Date: 2016/11/21 11:12
+ * @Copyright(©) 2015 by xiaomo.
+ **/
+
+public enum LinkView {
+
+    LOGIN("login"),
+    REGISTER("register"),
+    REGISTER_INFO("info"),
+    INDEX("/web/index");
+
+    private String name;
+
+    LinkView(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

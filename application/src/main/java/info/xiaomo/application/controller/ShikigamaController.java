6ee8c3ef6debc3601b4d2e18f3e27af86dfa1ee2
xiaomo <83387856@qq.com>
2016-12-24 18:10:44 +0800

    御魂

--- application/src/main/java/info/xiaomo/application/controller/ShikigamaController.java
+++ application/src/main/java/info/xiaomo/application/controller/ShikigamaController.java
@@ -5,7 +5,10 @@ import info.xiaomo.application.service.ShikigamaService;
 import info.xiaomo.core.constant.Err;
 import info.xiaomo.core.controller.BaseController;
 import info.xiaomo.core.controller.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +21,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/shikigami")
+@Api(value = "ShikigamaController",description = "阴阳师式神相关api")
 public class ShikigamaController extends BaseController {
 
     private final ShikigamaService shikigamaService;
@@ -30,6 +34,7 @@ public class ShikigamaController extends BaseController {
 
 
     @RequestMapping(value = "/findAll", method = RequestMethod.GET)
+    @ApiOperation(value = "查找所有式神", notes = "查找所有式神", httpMethod = "GET", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Result<List> findAll() {
         List<ShikigamiModel> all = shikigamaService.findAll();
         if (all == null || all.size() == 0) {
---
+++ crawler/src/main/java/info/xiaomo/crawler/model/MitamaModel.java
@@ -0,0 +1,55 @@
+package info.xiaomo.crawler.model;
+
+import info.xiaomo.crawler.model.base.BaseModel;
+
+/**
+ * @author 小莫 (https://xiaomo.info) (https://github.com/syoubaku)
+ * @created : 2016/12/24 18:06
+ * 御魂模型
+ */
+public class MitamaModel extends BaseModel {
+
+    /**
+     * 名字
+     * 火灵
+     */
+    private String name;
+
+    /**
+     * icon
+     * http://uus-ng.img.d.cn/snapshot/201610/999/image/388/388/hd/20161012151646257.jpeg
+     */
+    private String image;
+
+    /**
+     * 两件套效果
+     * 效果命中+15%
+     */
+    private String effect2;
+
+    /**
+     * 四件套效果
+     * 初次出场时获得额外3点鬼火（新回目战斗开始也会被计为初次出场）
+     */
+    private String effect4;
+
+    /**
+     * 关键字
+     */
+    private String keyworld;
+
+    /**
+     * 推荐式神
+     * 青行灯
+     */
+    private String suggest;
+
+
+    /**
+     * 获取方式
+     * 神秘商店、周末御魂
+     */
+    private String getWay;
+
+
+}

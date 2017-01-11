c7cb06d75a68015432a7da638727343d8eac92cf
xiaomo <83387856@qq.com>
2017-01-11 17:44:26 +0800

    添加api

--- aries/src/main/java/info/xiaomo/aries/controller/UserController.java
+++ aries/src/main/java/info/xiaomo/aries/controller/UserController.java
@@ -3,8 +3,11 @@ package info.xiaomo.aries.controller;
 import info.xiaomo.aries.model.UserModel;
 import info.xiaomo.aries.service.UserService;
 import info.xiaomo.core.controller.Result;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,7 +29,8 @@ public class UserController {
     }
 
 
-    @RequestMapping("findAll")
+    @RequestMapping(value = "findAll", method = RequestMethod.GET)
+    @ApiOperation(value = "返回所有用户数据", notes = "返回所有用户数据", httpMethod = "GET", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Result<List<UserModel>> findAll() {
         List<UserModel> all = service.findAll();
         return new Result<>(all);

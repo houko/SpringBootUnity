eb3884d113c3bee78a85f14b24cea42bcd8ca969
xiaomo <83387856@qq.com>
2017-01-13 09:58:21 +0800

    登录接口

---
+++ core/src/main/java/info/xiaomo/core/base/AuthOperate.java
@@ -0,0 +1,41 @@
+package info.xiaomo.core.base;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Date;
+
+/**
+ * @author 小莫 (https://xiaomo.info) (https://github.com/syoubaku)
+ * @version : 2017/1/13 9:51
+ */
+public interface AuthOperate {
+
+
+    /**
+     * 登录
+     *
+     * @param name     用户名(邮箱或者电话)
+     * @param password 密码
+     * @return 结果
+     */
+    Result<Boolean> login(@PathVariable String name, @PathVariable String password);
+
+    /**
+     * 注册
+     *
+     * @param name     用户名(邮箱或者电话)
+     * @param password 密码
+     * @return 是否己发送验证码
+     */
+    Result<Boolean> register(@PathVariable String name, @PathVariable String password);
+
+    /**
+     * 验证
+     *
+     * @param validCode 验证码
+     * @param time      发送时间
+     * @return 是否验证通过
+     */
+    Result<Boolean> validate(@PathVariable int validCode, @PathVariable Date time);
+
+}

3ee8ea924c89bbc0061fb77daf2aea83b2384e33
xiaomo <xiaomo@xiaomo.info>
2017-07-03 14:26:15 +0800

    版本更新

--- socket/src/main/java/info/xiaomo/chat/ChatMain.java
+++ socket/src/main/java/info/xiaomo/chat/ChatMain.java
@@ -3,6 +3,8 @@ package info.xiaomo.chat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -23,7 +25,7 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
  * Copyright(©) 2015 by xiaomo.
  **/
 @Configuration
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @ComponentScan("info.xiaomo")
 @EntityScan("info.xiaomo.*.model")
 public class ChatMain {

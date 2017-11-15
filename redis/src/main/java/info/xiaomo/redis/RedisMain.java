23c19f92fad3071853f8e80e013bcdba1864494d
xiaomo <xiaomo@xiaomo.info>
2017-11-15 11:22:24 +0800

    redis

--- redis/src/main/java/info/xiaomo/redis/RedisMain.java
+++ redis/src/main/java/info/xiaomo/redis/RedisMain.java
@@ -2,6 +2,8 @@ package info.xiaomo.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -22,7 +24,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * Copyright(©) 2015 by xiaomo.
  **/
 @Configuration
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @ComponentScan("info.xiaomo")
 @EnableCaching
 @EnableScheduling

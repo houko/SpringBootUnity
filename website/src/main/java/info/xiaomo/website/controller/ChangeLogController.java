c6b70f31449544bf7190dd359110ab5702fe24f7
xiaomo <xiaomo@xiaomo.info>
2018-03-02 20:07:19 +0800

    controller

--- website/src/main/java/info/xiaomo/website/controller/ChangeLogController.java
+++ website/src/main/java/info/xiaomo/website/controller/ChangeLogController.java
@@ -12,7 +12,11 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -78,7 +82,7 @@ public class ChangeLogController extends BaseController {
         if (model == null) {
             return new Result(CodeConst.NULL_DATA.getResultCode(), CodeConst.NULL_DATA.getMessage());
         }
-        return new Result(model);
+        return new Result<>(model);
     }
 
     /**

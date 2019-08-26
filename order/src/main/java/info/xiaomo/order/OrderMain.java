d1b3764485560dc515dca1aa1bb3ebdf1bb01437
胡朋（小莫） <xiaomo@xiaomo.info>
2019-08-27 00:02:39 +0900

    添加zx解码

--- order/src/main/java/info/xiaomo/order/OrderMain.java
+++ order/src/main/java/info/xiaomo/order/OrderMain.java
@@ -6,13 +6,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -42,17 +39,12 @@ public class OrderMain implements WebMvcConfigurer {
         SpringApplication.run(OrderMain.class, args);
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index() {
-        return "swagger-ui.html";
-    }
-
     /**
      * 接口
      *
      * @return 接口
      */
-    @RequestMapping(value = "/api", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     @ApiIgnore()
     @ApiOperation(value = "重定向到api首页")
     public ModelAndView api() {
